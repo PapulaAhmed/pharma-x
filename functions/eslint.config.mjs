a915c1bb3298f72a7577a30a5b13dd48a0fc1e3b
SahandMohammed <108280493+SahandMohammed@users.noreply.github.com>
2024-06-11 09:28:06 +0000

    changes before master change

---
+++ functions/eslint.config.mjs
@@ -0,0 +1,13 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import { fixupConfigRules } from "@eslint/compat";
+
+
+export default [
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  pluginJs.configs.recommended,
+  { files: ["**/*.jsx"], languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  ...fixupConfigRules(pluginReactConfig),
+];
